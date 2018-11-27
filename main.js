6c3b7590d8cdec57c669ca1b1f5374aec2f2d149
Hunter Metts <huntermettsdev@gmail.com>
2018-11-27 08:26:25 -0600

    Overly Excited | All exercises and parameters met

--- main.js
+++ main.js
@@ -8,8 +8,7 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray, x) {
-
+let addExcitement = (theWordArray, x) => {
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
